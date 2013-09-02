582afc1df60f3762ff3ba4b384cc1fdca3acad5a
alessandro-aglietti <alessandro.aglietti@gmail.com>
2013-09-02 13:13:57 +0200

    completato client

--- iti-note-rest-client-java/src/iti/note/test/client/TaccuinoClientTest.java
+++ iti-note-rest-client-java/src/iti/note/test/client/TaccuinoClientTest.java
@@ -1,40 +1,24 @@
 package iti.note.test.client;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import iti.note.model.Taccuino;
 
 import java.util.List;
 
-import javax.ws.rs.core.MediaType;
-
 import org.junit.Test;
-
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.GenericType;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.api.json.JSONConfiguration;
 
 public class TaccuinoClientTest {
 
 	@Test
 	public void test() {
-		DefaultClientConfig cc = new DefaultClientConfig();
-		cc.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING,
-				Boolean.TRUE);
+		Taccuino t = new Taccuino("Taccuino by Jersey");
 
-		Client client = Client.create(cc);
-
-		WebResource r = client
-				.resource("http://localhost:8080/iti-note-model-service/taccuino");
-
-		Taccuino t = r.type(MediaType.APPLICATION_JSON).post(Taccuino.class,
-				new Taccuino("Taccuino by Jersey"));
+		t.create();
 
 		assertNotNull(t.getId());
 
-		List<Taccuino> tt = r.get(new GenericType<List<Taccuino>>() {
-		});
+		List<Taccuino> tt = Taccuino.retrieve();
 
 		assertFalse(tt.isEmpty());
 	}
