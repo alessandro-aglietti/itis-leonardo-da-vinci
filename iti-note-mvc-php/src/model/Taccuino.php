68ec389fa134ada33b852d31844561fa98864d70
Neri Barnini <admin@queeply.com>
2013-12-29 18:23:01 +0100

    Modificato schema

--- iti-note-mvc-php/src/model/Taccuino.php
+++ iti-note-mvc-php/src/model/Taccuino.php
@@ -1,36 +1,35 @@
 <?php
 
 namespace ITI\Model;
+use Doctrine\Common\Collections\ArrayCollection;
 
-/**
- * @Entity
- * @Table(name="taccuino")
- **/
-class Taccuino {
-	
-	/** @Id
-	 * @Column(type="integer") 
-	 * @GeneratedValue(strategy="SEQUENCE")
-	 * @SequenceGenerator(sequenceName="hibernate_sequence")
-	**/
-	public $id;
-	
-	/** @Column(type="string") **/
-	public $titolo;
-	
-	/**
-	 * @OneToMany(targetEntity="ITI\Model\Nota", mappedBy="taccuino_id", cascade={"all"})
-	 * @var Nota[]
-	 **/
-	public $note;
-	
-	function __construct($titolo, $id = null){
-		$this->titolo = $titolo;
-		$this->id = $titolo;
-	}
-	
-	function setNote($note) {
-		$this->note = $note;
-	}
+/** @Entity **/
+class Taccuino
+{
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    public $id;
+
+    /** @Column(type="string") **/
+    public $titolo;
+
+    /**
+     * @OneToMany(targetEntity="Nota", mappedBy="taccuino", cascade={"persist"})
+     **/
+    private $notes;
+    // ...
+
+    public function __construct($titolo = '') {
+        $this->titolo    = $titolo;
+        $this->notes = new ArrayCollection();
+    }
+
+    public function setNotes($notes) {
+        $this->notes = $notes;
+    }
+
+    public function getNotes() {
+        return $this->notes;
+    }
 }
+
 ?>
